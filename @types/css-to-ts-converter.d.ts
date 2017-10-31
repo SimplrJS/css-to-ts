06b40945795e10bab43bfe728f374fff925afd5f
Deividas Bakanas <deividas@quatrodev.com>
2017-10-31 19:00:02 +0200

    Packages updated.

--- @types/css-to-ts-converter.d.ts
+++ @types/css-to-ts-converter.d.ts
@@ -6,7 +6,7 @@ export declare class CssToTsConverter {
     private varName;
     private header;
     private removeSource;
-    constructor(tsDir: string, tsFileName: string, cssDir: string, cssFileName: string, varName: string, header?: string, removeSource?: boolean);
+    constructor(tsDir: string, tsFileName: string, cssDir: string, cssFileName: string, varName: string, header?: string | undefined, removeSource?: boolean | undefined);
     Convert(): Promise<void>;
     private makeDirRecursively(dirPath);
 }
