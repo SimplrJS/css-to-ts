cac95ced3780379494589e795e4496d199c8e3ff
Deividas Bakanas <deividas@quatrodev.com>
2017-11-06 20:28:45 +0200

    `string`  type added to exported variable. New line is removed if it's in the end of the css content for every processed css.

--- src/css-to-ts.ts
+++ src/css-to-ts.ts
@@ -7,13 +7,18 @@ export function ConvertCssToTs(
     headerComment?: string,
     varType: VarType = VarType.Const
 ): string {
+    const newLineSplitterLength = EOL.length;
+    const newLineLastAppearance = stringifiedCss.lastIndexOf(EOL);
+    const endsWithNewLine = (newLineLastAppearance + newLineSplitterLength) === stringifiedCss.length;
+    const cssContent = endsWithNewLine ? stringifiedCss.slice(0, newLineLastAppearance) : stringifiedCss;
+
     let content = "";
 
     if (headerComment) {
         content += `// ${headerComment}${EOL}`;
     }
 
-    content += `export ${varType} ${variableName} = \`${stringifiedCss}\`;${EOL}`;
+    content += `export ${varType} ${variableName}: string = \`${cssContent}\`;${EOL}`;
 
     return content;
 }
